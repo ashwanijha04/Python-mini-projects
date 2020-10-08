1a56064b42d77451697196ba2f08edb729444f14
Ashwani Jha <ashwani@Ashwanis-MacBook-Air.local>
2020-10-08 13:14:35 +0530

    Changed profanityeditor,py

--- profanityeditor.py
+++ profanityeditor.py
@@ -1,20 +1,21 @@
 import os
-import urllib
+import urllib.request
+import urllib.parse
 
 def read_text():
-	quotes = open(os.path.expanduser("~/Desktop/movie_quotes.txt"))
+	quotes = open(os.path.expanduser("movie_quotes.txt"))
 	contents_of_file = quotes.read()
-	print contents_of_file
+	print (contents_of_file + "\n")
 	quotes.close()
 	check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
-	connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+	connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q=" + urllib.parse.quote(text_to_check, safe=''))
 	output = connection.read()
 	connection.close
-	if "true" in output:
-		print " Profanity Alert!"
+	if "true" in str(output):
+		print ("Profanity Alert!")
 	else:
-		print " Profanity free text!"
+		print ("Profanity free text!")
 
 read_text()
