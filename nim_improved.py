62a3630e17f945ce0fa9ca1cad37f0e5bfeb7172
Ashwani Jha <ashwani@Ashwanis-MacBook-Air.local>
2020-10-08 12:59:10 +0530

    Changed nim_improved.py

--- nim_improved.py
+++ nim_improved.py
@@ -1,33 +1,33 @@
 # Game of Nim
 
 def welcomeMessage():
-    print\
- """
+    print(
+"""
 This is a 2 player game.
 The computer-human based game will be made soon.
 There are a total of 13 sticks.
 Each player choses minimum 1 and maximum 4 sticks in one turn.
 The player who picks up the last stick wins.
 All the best to both players.
-"""
+""")
 
 def get_players():
-    player1 = raw_input("What is player 1's name?")
-    player2 = raw_input("What is player 2's name?")
+    player1 = input("What is player 1's name? ")
+    player2 = input("What is player 2's name? ")
     return player1, player2
-    
+
 def gameLogic():
     totalSticks = 13
     numberOfSticksPicked = 0
     turn = 1
     while totalSticks:
-        print"Number of sticks remaining: ", totalSticks
-        print "Player", turn, "'s turn."
-        numberOfSticksPicked = int(raw_input("Pick minimum 1 and maximum 4 sticks.\n"))
+        print ("Number of sticks remaining: ", totalSticks)
+        print ("Player", turn, "'s turn.")
+        numberOfSticksPicked = int(input("Pick minimum 1 and maximum 4 sticks.\n"))
         if numberOfSticksPicked > totalSticks\
         or numberOfSticksPicked > 4\
         or numberOfSticksPicked < 1:
-            print"That is not allowed. Try picking again."
+            print ("That is not allowed. Try picking again.")
             continue
         totalSticks = totalSticks - numberOfSticksPicked
         if turn == 1:
@@ -42,11 +42,11 @@ def gameLogic():
 
 def winMessage(turn,player1,player2):
     if turn:
-        print player2 + " won."
+        print (player2 + " won.")
     else:
-        print player1 + " won."
-    
-    
+        print (player1 + " won.")
+
+
 
 def main():
     welcomeMessage()
